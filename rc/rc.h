vshaden/wl500g

commit 8bf66550bab95b7337bd3fbc394c58e3e0b8a700

3g/cdma: Improve device detection (by rss), turn on 3g/cdma by default
git-svn-id: http://wl500g.googlecode.com/svn/trunk@1619 c3c545f6-d8d2-11dd-93da-dbb4cac96d7c

rc/rc.h
@@ -165,11 +165,17 @@ int stop_wimax(void);
 int madwimax_check(void);
 int madwimax_main(int argc, char **argv);
 int wimax_ifunit(char *ifname);
+int hotplug_check_wimax( char * interface, char * product, char * prefix );
 #endif
 
 #ifdef __CONFIG_MODEM__
 int start_modem_dial( char * prefix );
 int stop_modem_dial();
+int hotplug_check_modem( char * interface, char * product, char * prefix );
+#endif
+
+#if defined(__CONFIG_MADWIMAX__) || defined(__CONFIG_MODEM)
+void hotplug_network_device( char * interface, char * action, char * product );
 #endif
 
 #endif /* _rc_h_ */

rc/services_ex.c
@@ -38,6 +38,7 @@
 #include <mntent.h>
 #include "iboxcom.h"
 #include "lp.h"
+#include "nvparse.h"
 
 #define logs(s) syslog(LOG_NOTICE, s)
 
@@ -1642,21 +1643,11 @@ hotplug_usb(void)
 
 	if ((product=getenv("PRODUCT")))
 	{
-#ifdef __CONFIG_MADWIMAX__
-		/* wimax modem */
+#if defined(__CONFIG_MADWIMAX__) || defined(__CONFIG_MODEM__)
+		/* communication device */
 		if (strncmp(interface, "255/", 4) == 0)
 		{
-			if (strncmp(product, "4e8/6761", 8) == 0 ||
-			    strncmp(product, "4e9/6761", 8) == 0 ||
-			    strncmp(product, "4e8/6731", 8) == 0 ||
-			    strncmp(product, "4e8/6780", 8) == 0)
-			{
-				if (strcmp(action, "add") == 0)
-					nvram_set("usb_wimax_device", product);
-				else
-					nvram_unset("usb_wimax_device");
-				return 0;
-			}
+			hotplug_network_device( interface, action, product );
 		}
 #endif
 		/* usb storage */

rc/usbmodem.c
@@ -28,8 +28,11 @@ void modem_get_script_name( char * filename )
 int
 start_modem_dial( char * prefix )
 {
-    int ret;
+    int ret=0;
     char sfn[200];
+    char dialparam[] = "dial_param_XXXXXXXXX";
+    pid_t pid;
+    char tmp[200];
     char *argv[] = {
 	(modem_get_script_name(sfn), sfn),
 	NULL
@@ -37,24 +40,22 @@ start_modem_dial( char * prefix )
 
     dprintf( "%s", sfn );
 
-    char dialparam[] = "dial_param_XXXXXXXXX";
-    pid_t pid;
-    char tmp[200];
+    if( nvram_match( strcat_r(prefix, "dial_enabled", tmp), "1" ) &&
+	nvram_match( strcat_r(prefix, "prepared", tmp), "1" ) &&
+	nvram_get( strcat_r(prefix, "usb_device", tmp) ) ){
 
-    snprintf(dialparam, 20, "dial_param_%s", nvram_safe_get("wan_modem_mode_x"));
-    nvram_unset(dialparam);
-    nvram_unset("wan_modem_dial_param_1");
+	snprintf(dialparam, 20, "dial_param_%s", nvram_safe_get("wan_modem_mode_x"));
+	nvram_unset(dialparam);
+	nvram_unset("wan_modem_dial_param_1");
 
-    //nvram_set("wan0_primary", "1");
-    //nvram_set("wan1_primary", "0");
-
-    if (nvram_match("wan_proto", "usbmodem") &&
-	nvram_invmatch("wan_modem_mode_x", "-1"))
-    {
-		nvram_set("wan0_ifname", nvram_safe_get(strcat_r(prefix, "modem_ifname", tmp)) );
-		nvram_set("wan0_dnsenable_x", "1");
+	if (nvram_match( strcat_r(prefix, "proto", tmp), "usbmodem") &&
+		nvram_invmatch("wan_modem_mode_x", "-1"))
+	{
+		nvram_set( strcat_r(prefix, "ifname", tmp) , nvram_safe_get(strcat_r(prefix, "modem_ifname", tmp)) );
+		nvram_set( strcat_r(prefix, "dnsenable_x", tmp), "1");
+	}
+	ret = _eval(argv, NULL, 0, &pid);
     }
-    ret = _eval(argv, NULL, 0, &pid);
 
     dprintf("done\n");
     return ret;
@@ -63,9 +64,71 @@ start_modem_dial( char * prefix )
 int
 stop_modem_dial(void)
 {
-	int ret = eval("killall", "dial");
-	ret = eval("killall", "pppd");
+    int ret;
+    char sfn[200], * tmp;
+    //eval("killall", "dial");
+    modem_get_script_name( sfn );
+    tmp = strrchr( sfn, '/' );
+    if( tmp ) tmp++;
+    else tmp = sfn;
 
-	dprintf("done\n");
-	return ret;
+    eval("killall", tmp);
+    ret = eval("killall", "pppd");
+
+    dprintf("done\n");
+    return ret;
+}
+
+int hotplug_check_modem( char * interface, char * product, char * prefix )
+{
+    int ret=0;
+    char tmp[200], *str1, *str2;
+    char stored_product[40];
+    int vid, pid;
+    char *argv[] = {
+	"/usr/ppp/zerocd",
+	NULL
+    };
+
+    str1 = nvram_safe_get( "wan_modem_vid" );
+    sscanf( str1, "%x", &vid );
+    str2 = nvram_safe_get( "wan_modem_pid" );
+    sscanf( str2, "%x", &pid );
+
+    sprintf( stored_product, "%x/%x", vid, pid );
+
+    dprintf( "stored: %s, found: %s", stored_product, product );
+
+    if( strncmp( product, stored_product, strlen(stored_product) )==0 ){
+	ret=1;
+    } else {
+	if( !*str1 ){
+	    strcpy( stored_product, product );
+	    str1 = strchr( stored_product, '/' );
+	    if( str1 ){
+		*str1=0; str1++;
+		str2 = strchr( str1, '/' );
+		if( str2 ) *str2=0;
+		sscanf( stored_product, "%x", &vid ); sscanf( str1, "%x", &pid );
+		sprintf( stored_product, "0x%04x", vid );
+//		nvram_set(strcat_r(prefix, "modem_vid", tmp), stored_product );
+		nvram_set( "wan_modem_vid",  stored_product );
+		sprintf( stored_product, "0x%04x", pid );
+//		nvram_set(strcat_r(prefix, "modem_pid", tmp), stored_product );
+		nvram_set( "wan_modem_pid",  stored_product );
+		
+		dprintf( "wrote: %04x:%04x", vid, pid );
+		ret=1;
+	    }
+	}
+    }
+
+    if( ret==0 ){
+	// check zerocd mode
+	_eval(argv, NULL, 0, &pid);
+    }
+
+    dprintf("done. ret %d", ret );
+
+    return ret;
 }

config/defconfig
@@ -72,7 +72,7 @@ CONFIG_IGMPPROXY=y
 CONFIG_SCSIIDLE=y
 CONFIG_USBMODESWITCH=y
 CONFIG_MADWIMAX=y
-CONFIG_MODEM=n
+CONFIG_MODEM=y
 
 #
 # Additional C libraries

rc/Makefile
@@ -20,7 +20,7 @@ CFLAGS	+= -I. -I$(TOP)/shared -I$(SRCBASE)/include -Wall -DASUS_EXT $(GLOBAL_OPT
 #CFLAGS	+= -g -DDEBUG
 #CFLAGS	+= -g -DDEBUG -DDEBUG_TO_SYSLOG
 CFLAGS	+= -s -O2 $(EXTRACFLAGS)
-LDFLAGS	+= -L$(TOP)/nvram -lnvram -L$(TOP)/shared -lshared -lcrypt -Wl,-rpath /lib
+LDFLAGS	+= -L$(TOP)/nvram -lnvram -L$(TOP)/shared -lshared -lcrypt -lpthread -Wl,-rpath /lib
 
 MODEL := $(patsubst -DMODEL_%,%,$(filter -DMODEL_%,$(GLOBAL_OPTIONS)))
 STAMP := .stamp-$(MODEL)

rc/common_ex.c
@@ -24,6 +24,7 @@
 #include<syslog.h>
 #include <stdarg.h>
 #include <errno.h>
+#include "nvparse.h"
 
 #include "rc.h"
 
@@ -404,6 +405,20 @@ void convert_asus_values()
 	nvram_set("wan_dns_t", "");
 	nvram_set("wan_status_t", "Disconnected");
 
+#if defined( __CONFIG_MADWIMAX__ ) || defined ( __CONFIG_MODEM__ )
+	char tmp[100], prefix[] = "wanXXXXXXXXXX_";
+	int unit;
+	for (unit = 0; unit < MAX_NVPARSE; unit ++) 
+	{
+		snprintf(prefix, sizeof(prefix), "wan%d_", unit);
+		nvram_unset(strcat_r(prefix, "dial_enabled", tmp));
+		nvram_unset(strcat_r(prefix, "wimax_enabled", tmp));
+		nvram_unset(strcat_r(prefix, "prepared", tmp));
+		nvram_unset(strcat_r(prefix, "usb_device", tmp));
+		nvram_unset(strcat_r(prefix, "usb_device_name", tmp));
+	}
+#endif
+
 	if (nvram_match("wan_proto", "pppoe") || nvram_match("wan_proto", "pptp") ||
 		nvram_match("wan_proto", "l2tp"))
 	{
@@ -464,8 +479,8 @@ void convert_asus_values()
 		/* current interface address (dhcp + firewall) */
 		nvram_set("wanx_ipaddr", nvram_safe_get("wan_ipaddr"));
 		if( !nvram_match("wan_modem_enable_x", "1") )
-			nvram_unset( "wan0_modem_enable" );
-		else nvram_set( "wan0_modem_enable", "1" );
+			nvram_unset( "wan0_dial_enabled" );
+		else nvram_set( "wan0_dial_enabled", "1" );
 	}
 #endif
 
@@ -487,9 +502,6 @@ void convert_asus_values()
 	nvram_set("wan0_dnsenable_x", nvram_safe_get("wan_dnsenable_x"));
 	nvram_unset("wan0_dns");
 	nvram_unset("wanx_dns");
-#ifdef __CONFIG_MADWIMAX__
-	nvram_unset("wan0_wimax_enabled");
-#endif
 
 	convert_routes();
 

rc/network.c
@@ -43,9 +43,46 @@ typedef u_int8_t u8;
 #include <bcmutils.h>
 #include <etioctl.h>
 #include <bcmparams.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+sem_t * hotplug_sem;
 
 void lan_up(char *lan_ifname);
 int wait_for_ifup( char * prefix, char * wan_ifname, struct ifreq * ifr );
+
+void
+hotplug_sem_open()
+{
+	hotplug_sem = sem_open( "/hotplug_sem", O_CREAT | O_EXCL, S_IRWXU | S_IRWXG, 1 );
+	if( hotplug_sem == SEM_FAILED ){
+#ifdef DEBUG
+		dprintf( "%p, %s", hotplug_sem, strerror(errno) );
+#endif
+		hotplug_sem = sem_open( "/hotplug_sem", 0 );
+	}
+#ifdef DEBUG
+	dprintf( "%p, %s", hotplug_sem, strerror(errno) );
+#endif
+}
+
+void
+hotplug_sem_close()
+{
+	if(hotplug_sem) sem_close( hotplug_sem );
+}
+
+void
+hotplug_sem_lock()
+{
+	if(hotplug_sem) sem_wait( hotplug_sem );
+}
+
+void
+hotplug_sem_unlock()
+{
+	if(hotplug_sem) sem_post( hotplug_sem );
+}
 
 static int
 add_routes(char *prefix, char *var, char *ifname)
@@ -581,6 +618,10 @@ start_wan(void)
 	if (nvram_match("router_disable", "1"))
 		return;
 
+#if defined(__CONFIG_MADWIMAX__) || defined( __CONFIG_MODEM__)
+	hotplug_sem_open();
+#endif
+
 #ifdef ASUS_EXT
 	update_wan_status(0);
 	/* start connection independent firewall */
@@ -899,18 +940,20 @@ start_wan(void)
 				// start multicast router 
 				start_igmpproxy(wan_ifname);
 			}*/
+
+			hotplug_sem_lock();
+			nvram_set( strcat_r(prefix, "prepared", tmp), "1" );
 			
-			if( nvram_match( "wan0_modem_enable", "1" ) )
+			if( nvram_match( strcat_r(prefix, "dial_enabled", tmp), "1" ) )
 			{
-				// wait for USB initialization
-				sleep( 5 );
 				/* launch ppp client daemon */
 				start_modem_dial(prefix);
 			} else
 			{
 				demand=0;
-				nvram_set( "wan0_modem_enable", "1" );
+				nvram_set( strcat_r(prefix, "dial_enabled", tmp), "1" );
 			}
+			hotplug_sem_unlock();
 
 			/* ppp interface name is referenced from this point on */
 			wan_ifname = nvram_safe_get(strcat_r(prefix, "modem_ifname", tmp));
@@ -1047,6 +1090,11 @@ start_wan(void)
 		}
 	}
 #endif
+
+#if defined(__CONFIG_MADWIMAX__) || defined( __CONFIG_MODEM__)
+	hotplug_sem_close();
+#endif
+
 }
 
 void
@@ -1694,3 +1742,70 @@ int wait_for_ifup( char * prefix, char * wan_ifname, struct ifreq * ifr )
 	_eval(ping_argv, NULL, 0, &pid);
 	return 1;
 }
+
+#if defined(__CONFIG_MADWIMAX__) || defined(__CONFIG_MODEM)
+void hotplug_network_device( char * interface, char * action, char * product )
+{
+	char *wan_ifname;
+	char *wan_proto;
+	int unit;
+	char tmp[100], prefix[] = "wanXXXXXXXXXX_";
+
+	dprintf( "%s %s %s", interface, action, product );
+
+	int action_add = (strcmp(action, "add") == 0);
+
+	hotplug_sem_open();
+
+	/* Start each configured and enabled wan connection and its undelying i/f */
+	for (unit = 0; unit < MAX_NVPARSE; unit ++) 
+	{
+		snprintf(prefix, sizeof(prefix), "wan%d_", unit);
+
+		/* make sure the connection exists and is enabled */ 
+		wan_ifname = nvram_get(strcat_r(prefix, "ifname", tmp));
+		if (!wan_ifname)
+			continue;
+
+		wan_proto = nvram_get(strcat_r(prefix, "proto", tmp));
+		if (!wan_proto || !strcmp(wan_proto, "disabled"))
+			continue;
+
+		dprintf("%s %s %p\n\n\n\n\n", wan_ifname, wan_proto, hotplug_sem );
+
+		hotplug_sem_lock();
+#ifdef __CONFIG_MADWIMAX__
+		if (strcmp(wan_proto, "wimax") == 0)
+		{
+			if( hotplug_check_wimax( interface, product, prefix ) ){
+				if ( action_add ){
+					nvram_set(strcat_r(prefix, "usb_device", tmp), product );
+					start_wimax( prefix );
+				}
+			}
+		}
+#endif
+#ifdef __CONFIG_MODEM__
+		if (strcmp(wan_proto, "usbmodem") == 0)
+		{
+			if( hotplug_check_modem( interface, product, prefix ) ){
+				if ( action_add ){
+					nvram_set(strcat_r(prefix, "usb_device", tmp), product );
+					start_modem_dial( prefix );
+				}
+			}
+		}
+#else
+		{}
+#endif
+		hotplug_sem_unlock();
+
+		if( action_add == 0 )
+			nvram_unset(strcat_r(prefix, "usb_device", tmp) );
+	}
+
+	hotplug_sem_close();
+
+	dprintf("done");
+};
+#endif

rc/wimax.c
@@ -24,7 +24,8 @@ void update_nvram_wmx(char * ifname, int isup);
 inline static int
 wimax_modem(void)
 {
-	return nvram_invmatch("usb_wimax_device", "");
+	char * tmp = nvram_get("wan0_usb_device");
+	return ( tmp!=0 && *tmp!=0 );
 }
 
 inline static int
@@ -384,6 +385,23 @@ update_nvram_wmx(char *ifname, int isup)
 	else
 		nvram_set("wan_wimax_ping_t", "Disabled");
 }
+
+int 
+hotplug_check_wimax( char * interface, char * product, char * prefix )
+{
+	char tmp[100];
+
+	dprintf( "%s %s %s", interface, product, prefix );
+	if (strncmp(product, "4e8/6761", 8) == 0 ||
+	    strncmp(product, "4e9/6761", 8) == 0 ||
+	    strncmp(product, "4e8/6731", 8) == 0 ||
+	    strncmp(product, "4e8/6780", 8) == 0){
+		nvram_set(strcat_r(prefix, "usb_device_name", tmp), "Samsung CMC-730 chip");
+		return 1;
+	}
+	else
+		return 0;
+};
 
 int
 madwimax_main(int argc, char **argv)

commit 3ac7255387c4d2f7622174dba22c9ed9d220736c

3g/cdma: Fix for zerocd, broken in r1619, by rss.

cdma/files/usr/ppp/zerocd
@@ -14,4 +14,6 @@ else
  zerocddir="/usr/ppp/zerocdconf/${zerocddir}"
 fi
 
+logger "usb_modeswitch -c ${zerocddir}/${FILE}"
+
 [ -f "${zerocddir}/${FILE}" ] && usb_modeswitch -c ${zerocddir}/${FILE}

rc/rc.h
@@ -176,6 +176,7 @@ int hotplug_check_modem( char * interface, char * product, char * prefix );
 
 #if defined(__CONFIG_MADWIMAX__) || defined(__CONFIG_MODEM)
 void hotplug_network_device( char * interface, char * action, char * product );
+void hotplug_usb_modeswitch( char * interface, char * action, char * product );
 #endif
 
 #endif /* _rc_h_ */

rc/services_ex.c
@@ -1655,7 +1655,9 @@ hotplug_usb(void)
 		{
 			char *scsi_host = getenv("SCSI_HOST");
 			int scsi_host_no = -1;
-
+#if defined(__CONFIG_MODEM__)
+			hotplug_usb_modeswitch( interface, action, product );
+#endif
 			if (scsi_host)
 				scsi_host_no = atoi(scsi_host);
 			if (strcmp(action, "add") == 0)

rc/usbmodem.c
@@ -85,10 +85,6 @@ int hotplug_check_modem( char * interface, char * product, char * prefix )
     char tmp[200], *str1, *str2;
     char stored_product[40];
     int vid, pid;
-    char *argv[] = {
-	"/usr/ppp/zerocd",
-	NULL
-    };
 
     str1 = nvram_safe_get( "wan_modem_vid" );
     sscanf( str1, "%x", &vid );
@@ -123,12 +119,22 @@ int hotplug_check_modem( char * interface, char * product, char * prefix )
 	}
     }
 
-    if( ret==0 ){
-	// check zerocd mode
-	_eval(argv, NULL, 0, &pid);
-    }
-
     dprintf("done. ret %d", ret );
 
     return ret;
+}
+
+void
+hotplug_usb_modeswitch( char * interface, char * action, char * product )
+{
+    int pid;
+    char *argv[] = {
+	"/usr/ppp/zerocd",
+	NULL
+    };
+
+    if( strcmp(action, "add") == 0 ){
+	// check zerocd mode
+	_eval(argv, NULL, 0, &pid);
+    }
 }